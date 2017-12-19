54da7d323ea5f546d2e8f3e91d8e573fd367c4fa
Thiago O. Lopes <lopes.th.o@gmail.com>
2017-12-19 20:06:11 -0200

    Take the Frame in file

--- src/RMSDdynamic/RMSDdynamic.java
+++ src/RMSDdynamic/RMSDdynamic.java
@@ -1,6 +1,16 @@
 package RMSDdynamic;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class RMSDdynamic {
-	public static void main(String [] args) {
-		
+	public static void main(String [] args) throws IOException {
+		ArrayList<Integer> frames = new ArrayList<Integer>();
+		frames.add(1);
+		frames.add(2);
+		Takeframes x = new Takeframes(frames, "/Users/thiagolopes/Desktop/vmdepin.xyz", 26);
+		ArrayList<Integer> y = x.takeFrameLines();
+		System.out.println(y);
 	}
+
 }
--- src/RMSDdynamic/Takeframes.java
+++ src/RMSDdynamic/Takeframes.java
@@ -1,13 +1,45 @@
 package RMSDdynamic;
-import java.util.LinkedList;
-import java.util.List;
-public class Takeframes{
-	int[] frames;
 
-	public Takeframes(int[] frames) {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Takeframes{
+	ArrayList<Integer> frames = new ArrayList<Integer>();
+	private String trajectName;
+	private int basis;
+
+	public Takeframes(ArrayList<Integer> frames, String trajectName, int basis) {
 		super();
 		this.frames = frames;
+		this.trajectName = trajectName;
+		this.basis = basis;
 	}
 
+	public ArrayList<Integer> takeFrameLines() throws IOException {
+		ArrayList<Integer> startLines = new ArrayList<Integer>();
+		InputStream is = new FileInputStream(this.trajectName);
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		String linha; 
+		int numLine = 1;
+		while((linha = br.readLine()) != null) {
+			String [] linhaS = linha.split("\\s++");
+			if (linhaS.length < 2) {
+				numLine ++;
+				continue;
+			}
+			if (linhaS[1].equals(Integer.toString(basis))){
+				startLines.add(numLine+2);
+			}
+			numLine ++;
+		}
+		br.close();
+		is.close();
+		isr.close();
+		return startLines;
+	}
 }
-	
