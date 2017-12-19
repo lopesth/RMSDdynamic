153eb40af54049174fe06f56138983f5c70d684a
Thiago O. Lopes <lopes.th.o@gmail.com>
2017-12-19 16:47:23 -0200

    atom class

---
+++ src/Atom.java
@@ -0,0 +1,37 @@
+public class Atom {
+	private String atomType;
+	private float xPos;
+	private float yPos;
+	private float zPos;
+	
+	public Atom(String atomType, float xPos, float yPos, float zPos) {
+		this.atomType = atomType;
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.zPos = zPos;
+	}
+	
+	public float[] getPOS() {
+		float[] pos = new float[3];
+		pos[1] = this.xPos;
+		pos[2] = this.yPos;
+		pos[3] = this.zPos;
+		return pos;
+	}
+
+	public float getxPos() {
+		return xPos;
+	}
+
+	public String getAtomType() {
+		return this.atomType;
+	}
+
+	public float getyPos() {
+		return yPos;
+	}
+
+	public float getzPos() {
+		return zPos;
+	}
+}
---
+++ src/RMSDdynamic.java
@@ -0,0 +1,6 @@
+
+public class RMSDdynamic {
+	public static void main(String [] args) {
+		
+	}
+}
